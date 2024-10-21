96420a14d34e3c84f883a7b54ad7dd4d18f56a11
farhan086 <patelfarhan8600@gmail.com>
2024-10-21 16:02:56 +0530

    house prediction

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import joblib 
+import numpy as np
+
+st.title("House prediction app")
+st.image("house prediction.jpg")
+
+House_size = st.number_input("Please enter House Size:")
+Bedrooms=st.number_input("Please enter No of Bedrooms:")
+
+model = joblib.load("mul_linear.pkl")
+ 
+if st.button("Predict"):
+    features = np.array([[House_size, Bedrooms]])
+    output = model.predict(features)
+    st.write(f"The price of the house is {output[0]}")
